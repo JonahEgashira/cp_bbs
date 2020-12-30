05cd247ffe8468735f077dc970541a961516aebe
Jonah Egashira <jonahega@gmail.com>
2020-12-30 12:22:19 +0900

    validate empty post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -16,4 +16,27 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+
+  def edit
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id:params[:id])
+    @post.content = params[:content]
+    
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      redirect_to("/posts/#{@post.id}/edit")
+    end
+
+  end
+
+  def destroy
+    @post = Post.find_by(id:params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+
 end
